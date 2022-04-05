58dfdfdf31163c8e95a772f32f0c41976e423e55
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-04-05 11:58:05 +0400

    task

---
+++ Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp42
+{
+    class Book
+    {
+        public Book(int no)
+        {
+            No = no;
+            _no++;
+        }
+        private static int _no;
+        public int No { get; set; }
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public Enum Genre;
+        public int Price { get; set; }
+    }
+}
