6cdcc45b6dead4d8553c75844cc28885c5bb042d
FeridShixaliyev <99806164+FeridShixaliyev@users.noreply.github.com>
2022-04-05 12:23:31 +0400

    task

---
+++ Library.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp42
+{
+    class Library
+    {
+        List<Book> books = new List<Book>();
+        public List<Book> FilterByPrice(int min, int max)
+        {
+            List<Book> filtered = new List<Book>();
+
+            var filteredprice = books.FindAll(x => x.Price >= min && x.Price <= max);
+
+            if(filteredprice!=null)
+                filtered.add
+
+
+        }
+
+        public List<Book> FilterByGenre()
+        {
+
+        }
+        public Book FİndBookByNo(int no)
+        {
+            
+
+            return null;
+        }
+    }
+}
